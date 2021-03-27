b37c78815eaabafc744db29e7ff64a26f3b4d5a0
KimJinsu66 <jinsu6688@gmail.com>
2021-03-27 18:37:11 +0900

    메소드 리팩터링 &

--- src/components/todo/TodoHeader.vue
+++ src/components/todo/TodoHeader.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="text-center">
-  
     <div class="my-3">
       <div class="input-group mb-3">
-        <span class="input-group-text input-group-box" id="inputGroup-sizing-default">Content</span>
         <input type="text" 
           class="form-control"
           placeholder="할일을 입력하세요"
@@ -13,13 +11,11 @@
       </div>
 
       <div class="input-group mb-3">
-        <span class="input-group-text input-group-box" id="inputGroup-sizing-default">Start</span>
-        <input type="datetime-local" class="form-control" v-model="startDate">  
-      </div>
-
-      <div class="input-group mb-3">
         <span class="input-group-text input-group-box" id="inputGroup-sizing-default">End</span>
-        <input type="datetime-local" class="form-control" v-model="endDate">
+        <button> 오늘
+          <input type="date" v-model="endDate" class="form-control" id="end-date-input">
+        </button>
+        <DateTimePicker :label="'시작날짜'"></DateTimePicker>
       </div>
 
       <div class="d-grid gap-2">
@@ -30,66 +26,94 @@
 </template>
 
 <script>
+  import DateTimePicker from "../common/DateTimePicker.vue";
   export default {
-    props: ['todoIndices'],
+    props: {
+      todoIndices: Array,
+    },
+
+    componnets: {
+      DateTimePicker
+    },
 
     data() {
       return {
-        index:     null,
-        content:   null,
-        createdAt: null,
-        updatedAt: null,
-        startDate: null,
-        endDate:   null,
+        index:        null,
+        content:      null,
+        createdAt:    null,
+        updatedAt:    null,
+        startDate:    null,
+        endDate:      null,
+        currentDate:  this.getCurrentDate(),
+        previousDate: this.getpreviousDate(),
       }
     },
   
     methods: {
       async createTodo () {
-        console.log("TOdo Header created in");
-        const content = this.content;
         
         if ( this.checkEmptyContent(this.content) ) {
           alert("할일을 입력하세요.");
           return
         }
-
-        const data  = this.setData(this.content, 
-                                   this.convertDateTime(this.startDate),
-                                   this.convertDateTime(this.endDate)
-                                  );
+        
+        const data  = this.setData(this.content, this.checkEmptyDate(this.endDate));
         this.$emit('createTodo', data);
-        console.log("Todo Header Creadeted end");
+        
+        this.initializeInput();
+      },
+
+      initializeInput () {
+        this.content   = null;
+        this.endDate   = null;
       },
 
       checkEmptyContent(content) {
-        const checkedContent = content == "" || content == null || content == undefined || 
+        const checkContent = content == "" || content == null || content == undefined || 
              ( content != null && typeof content == "object" && !Object.keys(content).length );
-        return checkedContent;
+        return checkContent;
       },
 
-      convertDateTime (dateTime) {
-        if (dateTime === null) return "";
-
-        return dateTime.replace("T", " ");
+      checkEmptyDate (dateTime) {
+        const checkDate = dateTime == "" || dateTime == null || dateTime == undefined || 
+             ( dateTime != null && typeof dateTime == "object" && !Object.keys(dateTime).length );
+        
+        if (checkDate) return this.getCurrentDate();
+        return dateTime;
       },
 
-      clearInput() {
-        console.log("Todo Header clear Input");
+      getCurrentDate() {
+        const date = new Date();
+        
+        return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
       },
 
-      setData (content, startDate, endDate) {
+      getpreviousDate() {
+        const date  = new Date();
+        const month = date.getMonth() < 10 ? '0' + (date.getMonth() + 1) : date.getMonth();
+        
+        return [date.getFullYear(), month, date.getDate() - 1].join('-');
+      },
+
+      setData (content, endDate) {
         return {
-          content:   content,
-          startDate: startDate,
-          endDate:   endDate
+          content: content,
+          endDate: endDate,
         };
       },
+
+      setMinAttribute () {
+        document.getElementById('end-date-input').setAttribute('min', this.previousDate);
+      }
+    },
+
+    mounted() {
+      console.log("good");
     },
   }
 </script>
 
-<style scope>
+<style scoped>
   .input-group-box {
     width: 100px;
   }
