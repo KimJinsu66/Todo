a4354d05407723fb106b28740d6fb447d4970203
KimJinsu66 <jinsu6688@gmail.com>
2021-03-30 14:35:14 +0900

    Project 파일, Upcoming 파일을 만들었습니다

---
+++ src/components/todo/TodoProject.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="w-100">
+    <div class="todo-main">
+      <div class="project-header">
+        <h5><strong>관리함</strong></h5>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import TodoSideBar from './TodoSideBar.vue'
+
+  export default {
+    name: 'todo-project',
+
+    components: {
+      'TodoSideBar': TodoSideBar,
+    }
+  }
+</script>
+
+<style lang="" scoped>
+  .project-header {
+    padding: 96px 0 0 0;
+  }
+</style>
---
+++ src/components/todo/TodoUpcoming.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="w-100">
+    <div class="todo-main">
+      <div class="upcoming-header">
+        <h5><strong>{{ currentMonth }}월 {{ currentYear }}</strong></h5>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import TodoSideBar from './TodoSideBar.vue'
+  export default {
+    name: 'todo-upcoming',
+    date () {
+      return {
+        currentYear:  null,
+        currentMonth: null,
+        currentDate:  null,
+      } 
+    },
+
+    components: {
+      'TodoSideBar': TodoSideBar,
+    },
+
+    created () {
+      this.getCurrentDate();
+    },
+
+    methods: {
+      getCurrentDate () {
+        const currentDate = new Date();
+        this.currentYear  = currentDate.getFullYear();
+        this.currentMonth = currentDate.getMonth() + 1;
+        this.currentDate  = currentDate.Date();
+      },
+    },
+
+  }
+</script>
+
+<style scoped>
+  .upcoming-header {
+    padding: 96px 0 0 0;
+  }
+</style>
