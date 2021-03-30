663dfd65baf4090a2166c804ee40d9f1c4e472f5
KimJinsu66 <jinsu6688@gmail.com>
2021-03-30 11:13:10 +0900

    TodoSidebar의 전체적인 디자인 틀의 코드를 작성하였습니다

--- src/components/todo/TodoSideBar.vue
+++ src/components/todo/TodoSideBar.vue
@@ -1,11 +1,30 @@
 <template lang="">
   <transition name="fade">
     <div class="sidebar-container" v-if="show">
-      <ul>
-        <li>관리함</li>
-        <li>오늘</li>
-        <li>다음</li>
-      </ul>
+      <div class="p-3 d-flex flex-column ">
+      
+        <router-link to="/" class="sidebar-link-box d-flex justify-content-between align-items-center">
+          🗄️ 관리함
+        </router-link>
+        <router-link to="/" class="sidebar-link-box d-flex justify-content-between align-items-center">
+          🗓️ 오늘
+        </router-link>
+        <router-link to="/" class="sidebar-link-box d-flex justify-content-between align-items-center">
+          📚 다음
+        </router-link>
+      </div>
+
+      <div>
+        <div class="additional-features-box">
+          > 프로젝트
+        </div>
+        <div class="additional-features-box">
+          > 라벨
+        </div>
+        <div class="additional-features-box">
+          > 필터
+        </div>
+      </div>
     </div>    
   </transition>  
 </template>
@@ -35,11 +54,44 @@
   }
 </script>
 
-<style lang="">
+<style scoped>
+  a {
+    color: black !important;
+    text-decoration: none;
+    height: 25px;
+    padding: 3px;
+    margin-bottom: 5px;
+    border-radius: 5px;
+    font-size: 1rem;
+  }
+
+  ul {
+
+  }
+
+  a:visited { 
+    color: white; 
+    text-decoration: none;
+  }
+
+  a:hover { 
+    background-color: #ede8e8;
+    color: white;
+    text-decoration: underline;
+  }
+
   .sidebar-container {
     width: 300px;
-    padding: 15px 0 0 15px;
+    padding: 30px 0 0 35px;
     background: #FAFAFA;
+  }
+
+  .sidebar-link-box {
+    padding: 20px 16px 20px 5px;
+  }
+
+  .additional-features-box {
+    padding: 16px 32px 16px 32px;
   }
 
   .fade-enter-active, .fade-leave-active {
