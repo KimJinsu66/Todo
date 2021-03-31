aa95f619eb3e155d284d41edcddc076fbca05204
KimJinsu66 <jinsu6688@gmail.com>
2021-03-31 16:33:36 +0900

    TodoSidebar를 추가하였습니다

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <template>
   <div id="app">
     <TodoNavbar></TodoNavbar>
-    <router-view/> <!-- router view에서 view를 가져온다-->
-
+    <div class="d-flex w-100 h-100">
+      <TodoSideBar></TodoSideBar>
+      <div class="w-100">
+        <div class="todo-main">
+          <router-view/> <!-- router view에서 view를 가져온다-->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  import TodoNavbar from './components/todo/TodoNavbar.vue'
+  import TodoNavbar  from './components/todo/TodoNavbar.vue'
+  import TodoSideBar from './components/todo/TodoSideBar.vue'
+  
   export default {
     name: 'App',
     components: {
-      'TodoNavbar': TodoNavbar,
-    }
+      'TodoNavbar':  TodoNavbar,
+      'TodoSideBar': TodoSideBar,
+    },
   }
 </script>
 
@@ -31,5 +40,11 @@
   .todo-container {
     margin: 0 auto;
     width: 700px;
+  }
+
+  .todo-main {
+    max-width: 800px;
+    width: 100%;
+    margin: 0 auto;
   }
 </style>
